14dc970937a0c49f6291d8e07b6438abe19962c7
“Felipe <felipescherer.ads@gmail.com>
2019-12-26 18:21:02 -0300

    ajustes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(`$/dist/$`));
 
 app.get("/*", (req, res) => {
   //res.sendFile(path.join(`$/dist/$/index.html`));
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile("dist/angular-hackernews/index.html");
 });
 
 app.listen(process.env.PORT || 8080);

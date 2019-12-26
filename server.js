d2e5c0b0fcd10ac752d0299d102477f56f2a0192
“Felipe <felipescherer.ads@gmail.com>
2019-12-26 17:57:49 -0300

    Ajuste cors

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const nomeApp = process.env.npm_package_name;
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.static(`$/dist/$`));
 
 app.get("/*", (req, res) => {
